7a861b2e408f3356ce9cbcffe92f74673bdc551b
abinayasplatform <abinayaraja1624@gmail.com>
2024-03-29 12:51:05 +0530

    Footerr added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
 
 function App(){
   return (
     <div>
 <Header/>
+
+<Footer/>
     </div>
   )
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-light py-4">
+      <div className="container">
+        <div className="row ">
+          <div className="col-md-6">
+            <p className="mb-0">
+              © 2024 All rights reserved By <span className="font-weight-bold">Abinaya Raja</span>.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
