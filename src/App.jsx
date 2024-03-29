1a92f02540ce06ca4c05b51844fee5340aa4d01f
abinayasplatform <abinayaraja1624@gmail.com>
2024-03-29 12:55:05 +0530

    Home added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import { UserContextProvider } from "./Components/Context/usercontext";
 
 
 function App(){
   return (
     <div>
 <Header/>
-
+<UserContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+         
+        </Routes>
+      </UserContextProvider>
 <Footer/>
     </div>
   )
